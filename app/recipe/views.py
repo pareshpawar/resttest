6bad301ca619401614a74f2c688f2f2ecb9c1119
Paresh Pawar <par20esh@gmail.com>
2019-12-08 15:19:04 +0530

    + create recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -58,3 +58,7 @@ class RecipeViewset(
             return RecipeDetailSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
